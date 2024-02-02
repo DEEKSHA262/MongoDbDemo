91a45fa6a376f1a49d1f879abbed1874564a0c16
DeekshaRamdas <DeekshaRamdas@DESKTOP-8MNPJIF>
2024-02-02 00:00:57 -0800

    Add project files.

---
+++ MongoDbDemo/Model.cs
@@ -0,0 +1,17 @@
+﻿
+
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbDemo;
+
+public class Model
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ MongoDbDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using MongoDbDemo;
+
+string connectionString = "mongodb://localhost:27017";
+string databaseName = "simple_db";
+string collectionName = "StudentRecord";
+
+var Client = new MongoClient(connectionString);
+IMongoDatabase db = Client.GetDatabase(databaseName);
+var collection = db.GetCollection<Model>(collectionName);
+
+var person1 = new Model { FirstName = "Deeksha", LastName = "Ramdhas", Age = 24};
+await collection.InsertOneAsync(person1);
+var person2 = new Model { FirstName = "Srujan", LastName = "Ramdhas", Age = 19};
+await collection.InsertOneAsync(person2);
+
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine(value:$"{result.Id}: {result.FirstName} {result.LastName} {result.Age} ");
+}
+
